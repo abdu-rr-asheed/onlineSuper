4ea6f2a5f942de40574cf3aa086fa9ba85bfdb8f
abdu-rr-asheed <abdurrasheed430@gmail.com>
2022-02-17 12:32:10 +0530

    card feching

--- src/containers/Cards.jsx
+++ src/containers/Cards.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import Heading from "../components/Heading";
 import Card from "../components/Card";
 
-const Cards = () => {
+import { products } from "../utilities/data";
+
+const Cards = ({ main_heading, slug_title }) => {
   return (
     <>
-      <Heading main_heading="New Arrivals" slug_title="Exclusive" />
+      <Heading main_heading={main_heading} slug_title={slug_title} />
       <div className="flex justify-around items-center flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {products.map((item, idx) => (
+          <React.Fragment key={idx}>
+            {item.recommend == 1 && (
+              <Card
+                name={item.name}
+                image={item.image}
+                weight={item.weight}
+                price={item.price}
+                discountPrice={item.discountPrice}
+                categoryId={item.categoryId}
+              />
+            )}
+          </React.Fragment>
+        ))}
       </div>
     </>
   );
--- src/utilities/data.jsx
+++ src/utilities/data.jsx
@@ -7,6 +7,8 @@ import {
 } from "react-icons/gi";
 import { BiDrink } from "react-icons/bi";
 
+import food3 from "../assets/food3.png";
+
 export const categories = [
   {
     id: 1,
@@ -49,5 +51,80 @@ export const categories = [
     icon: <GiMeat />,
     image:
       "https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    name: "kiwi",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[0].name,
+    recommend: 0,
+    newProducts: 1,
+    offers: 0,
+  },
+  {
+    id: 2,
+    name: "kiwi2",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[1].name,
+    recommend: 1,
+    newProducts: 1,
+    offers: 0,
+  },
+  {
+    id: 3,
+    name: "kiwi3",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[2].name,
+    recommend: 1,
+    newProducts: 1,
+    offers: 0,
+  },
+  {
+    id: 4,
+    name: "Kiwi4",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[3].name,
+    recommend: 1,
+    newProducts: 1,
+    offers: 0,
+  },
+  {
+    id: 5,
+    name: "kiwi5",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[4].name,
+    recommend: 1,
+    newProducts: 1,
+    offers: 0,
+  },
+  {
+    id: 6,
+    name: "kiwi5",
+    image: food3,
+    weight: "100g",
+    price: 1300,
+    discountPrice: 1200,
+    categoryId: categories[4].name,
+    recommend: 1,
+    newProducts: 1,
+    offers: 0,
   },
 ];
